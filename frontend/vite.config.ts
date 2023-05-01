3c3eb31f845185f02c7aae54ed0b5482a43fb582
hima7459 <violet@himaa.me>
2023-05-02 02:59:20 +0800

    env update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_IMAGE_VOLUME_URL': JSON.stringify(process.env.VITE_IMAGE_VOLUME_URL),
+    plugins: [react()],
+  },
+});
