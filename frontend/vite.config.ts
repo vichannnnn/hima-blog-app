fc0865c2941c18a1181ae08d0609135d75a6f0be
hima7459 <violet@himaa.me>
2023-05-02 03:02:31 +0800

    format error for vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
   define: {
     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
     'import.meta.env.VITE_IMAGE_VOLUME_URL': JSON.stringify(process.env.VITE_IMAGE_VOLUME_URL),
-    plugins: [react()],
   },
+  plugins: [react()],
 });
