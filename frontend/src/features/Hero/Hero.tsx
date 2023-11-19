513d316e2d9ee744cf16a9437c9ab3188f07baf1
vi.channnnn <violet@himaa.me>
2023-11-19 13:35:23 +0800

    feat: update typing to fade in for hero subtitle and update to bun

--- frontend/src/features/Hero/Hero.tsx
+++ frontend/src/features/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import { TypeAnimation } from 'react-type-animation';
+import FadeIn from 'react-fade-in';
 import './Hero.css';
 
 export const Hero = () => {
@@ -9,14 +10,11 @@ export const Hero = () => {
           <TypeAnimation sequence={["Welcome to Hima's Blog", 100]} speed={50} cursor={false} />
         </h1>
         <p className='hero-subtitle'>
-          <TypeAnimation
-            style={{ whiteSpace: 'pre-line', display: 'block' }}
-            sequence={[
-              'This is where the ramblings and yapping of engineering lies. It could be about work, personal projects, or anything really. I would ideally like to update the blog with new post every week to document down my engineering journey but we shall see how it goes! \n(Logo is temporary placeholder)',
-            ]}
-            speed={99}
-            cursor={false}
-          />
+          <FadeIn>
+            This is where the ramblings and yapping of engineering lies. It could be about work,
+            personal projects, or anything really. I would ideally like to update the blog with new
+            post every week to document down my engineering journey but we shall see how it goes!
+          </FadeIn>
         </p>
       </div>
     </div>
