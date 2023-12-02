8dfd8d5531749e3731849e7ea4cd87e438ba9f3c
vi.channnnn <violet@himaa.me>
2023-12-03 05:52:46 +0800

    feat(footer): add footer

--- frontend/src/features/Footer/Footer.tsx
+++ frontend/src/features/Footer/Footer.tsx
@@ -1,3 +1,13 @@
+import { Link as RouterLink } from 'react-router-dom';
+import './Footer.css';
+
 export const Footer = () => {
-  return <></>;
+  return (
+    <div className='footer-container'>
+      <RouterLink className='nav_logo' to='/'>
+        <img src='https://image.himaa.me/hima-chan-sitting.png' alt='' />
+      </RouterLink>
+      <p>© 2023 Hima • Questions? Contact me at violet@himaa.me</p>
+    </div>
+  );
 };
