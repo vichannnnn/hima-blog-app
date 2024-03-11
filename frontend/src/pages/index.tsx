227f8a99731213b713ed66c92b71f82acfbc04a3
vi.channnnn <violet@himaa.me>
2024-03-11 15:03:00 +0900

    chore: remove hero component and directly integrate static content into homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/pages/index.module.css';
 import { getAllBlogPosts, BlogPost as BlogPostObject } from '@api/blog';
 import { BlogPost } from '@components';
 import { MediaQueryContext, AuthContext } from '@providers';
-import { Hero } from '../components/Hero';
+import Image from "next/image";
 
 const HomePage = () => {
   const [blogPosts, setBlogPosts] = useState<BlogPostObject[]>([]);
@@ -45,7 +45,23 @@ const HomePage = () => {
 
   return (
     <>
-      <Hero />
+      <div className={styles.heroContainer}>
+        <Image
+            src='https://image.himaa.me/hima-chan-posing.png'
+            alt='Hima!'
+            width='225'
+            height='225'
+            loading='lazy'
+        />
+        <div>
+          <h1>Welcome to Hima&apos;s Engineering Blog</h1>
+          <p className={styles.heroSubtitle}>
+            This is where the ramblings and yapping of my software engineering journey lies. It could be about engineering-related stuff like work,
+            personal projects, or it could be anything! I would ideally like to update the blog with new
+            post every other week to document down my engineering journey but we shall see how it goes!
+          </p>
+        </div>
+      </div>
       <div className={styles.page}>
         {blogPosts.map((post, index) => (
           <BlogPost
